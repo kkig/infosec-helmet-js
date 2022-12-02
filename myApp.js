d16a7b44c72cc4d0e912dc81e4bbab4f9b3ce749
kkig <kigoshi.kanazawa@gmail.com>
2022-12-02 23:04:48 +0900

    Add .frameguard & xssFilter

--- myApp.js
+++ myApp.js
@@ -4,6 +4,8 @@ const app = express();
 const helmet = require('helmet');
 
 app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({action: 'deny'}));
+app.use(helmet.xssFilter());
 
 module.exports = app;
 const api = require('./server.js');
