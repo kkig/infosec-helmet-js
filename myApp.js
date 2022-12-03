9dc54bd1e4cbe59dce5cf58dce745f4bfdd021f0
kkig <kigoshi.kanazawa@gmail.com>
2022-12-03 23:51:22 +0900

    Add .ieNoOpen() to set x-download-options header

--- myApp.js
+++ myApp.js
@@ -7,6 +7,7 @@ app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter()); // not necessary when Content-Security-Policy is enabled
 app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
 
 module.exports = app;
 const api = require('./server.js');
