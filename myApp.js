3b39e1485c016a82eaf9c517c3ce2e324f494a12
kkig <kigoshi.kanazawa@gmail.com>
2022-12-05 00:33:24 +0900

    Refactoring code to add helmet middleware

--- myApp.js
+++ myApp.js
@@ -4,19 +4,29 @@ const app = express();
 const helmet = require('helmet');
 
 const hstsTimeInSec = 90 * 24 * 60 * 60; // 90 days
+const cspDirs = {
+  defaultSrc: ["'self'"],
+  scriptSrc: ["'self'", 'trusted-cdn.com'],
+};
 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
-app.use(helmet.xssFilter()); // not necessary when Content-Security-Policy is enabled
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-app.use(helmet.hsts({maxAge: hstsTimeInSec, force: true})); // Priotize https over http
-app.use(helmet.dnsPrefetchControl()); // Turn off X-DNS-Prefetch-Control to priotize security over performance
-app.use(helmet.noCache()); // Try to disable cache on client's browser - deprecated in latest helmet
+app.use(
+  helmet({
+    frameguard: {action: 'deny'},
+    hsts: {maxAge: hstsTimeInSec, force: true},
+    contentSecurityPolicy: {directives: cspDirs},
+  })
+);
 
-const dirs = {defaultSrc: ["'self'"], scriptSrc: ["'self'", 'trusted-cdn.com']};
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.frameguard({action: 'deny'}));
+// app.use(helmet.xssFilter()); // not necessary when Content-Security-Policy is enabled
+// app.use(helmet.noSniff());
+// app.use(helmet.ieNoOpen());
+// app.use(helmet.hsts({maxAge: hstsTimeInSec, force: true})); // Priotize https over http
+// app.use(helmet.dnsPrefetchControl()); // Turn off X-DNS-Prefetch-Control to priotize security over performance
+// app.use(helmet.noCache()); // Try to disable cache on client's browser - deprecated in latest helmet
 
-app.use(helmet.contentSecurityPolicy({directives: dirs}));
+// app.use(helmet.contentSecurityPolicy({directives: cspDirs}));
 
 module.exports = app;
 const api = require('./server.js');
